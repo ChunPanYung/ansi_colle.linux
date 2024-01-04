7de6353f59230ba08778e452b7ae78b2ebc0f60e
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-01-04 12:01:04 -0500

    feat: add testing of different directory.

--- roles/home_config/tests/test_linux_home.py
+++ roles/home_config/tests/test_linux_home.py
@@ -6,10 +6,18 @@ from testinfra.host import Host
 @pytest.mark.linux
 @pytest.mark.home_config
 class TestPackage:
-    @pytest.fixture
-    def get_home(self, host: Host):
-        return host.user("root").home
-
     def test_dir(self, host: Host, get_home: str):
-        assert host.file("/tmp/RAM").is_directory
-        assert host.file(get_home).is_directory
+        assert host.file(f"{get_home}/.bashrc.d").is_directory
+
+        assert host.file(f"{get_home}/.ssh").is_directory
+        assert host.file(f"{get_home}/.gnupg").is_directory
+        assert host.file(f"{get_home}/.keys").is_directory
+
+        assert host.file(f"{get_home}/.local/bin").is_directory
+        assert host.file(f"{get_home}/.local/opt").is_directory
+        assert host.file(f"{get_home}/.local/script").is_directory
+        assert host.file(f"{get_home}/.local/state").is_directory
+
+        assert host.file(f"{get_home}/.config/systemd/user/").is_directory
+
+        assert host.file(f"{get_home}/Repos/git").is_directory
