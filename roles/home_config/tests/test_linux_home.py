1d7f01822f41c99e908a70d39d751d435eab786c
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-01-04 14:00:28 -0500

    feat: add function to test file.

--- roles/home_config/tests/test_linux_home.py
+++ roles/home_config/tests/test_linux_home.py
@@ -6,7 +6,7 @@ from testinfra.host import Host
 @pytest.mark.linux
 @pytest.mark.home_config
 class TestPackage:
-    def test_dir(self, host: Host, get_home: str):
+    def test_home_dir(self, host: Host, get_home: str):
         assert host.file(f"{get_home}/.bashrc.d").is_directory
 
         assert host.file(f"{get_home}/.ssh").is_directory
@@ -21,3 +21,9 @@ class TestPackage:
         assert host.file(f"{get_home}/.config/systemd/user/").is_directory
 
         assert host.file(f"{get_home}/Repos/git").is_directory
+
+    def test_home_file(self, host: Host, get_home: str):
+        assert host.file(f"{get_home}/.bashrc").is_file
+        assert host.file(f"{get_home}/.gitignore").is_file
+        assert host.file(f"{get_home}/.gitconfig").is_file
+        assert host.file(f"{get_home}/.bash_profile").is_file
