48053cc87cd62e593cdbec204377078bac3ef1f4
ChunPanYung <31672021+ChunPanYung@users.noreply.github.com>
2024-01-11 20:41:13 -0500

    test: add and setup testinfra example. (#9)
    * refactor: remove ansible module and function test.
    
    * refactor: remove testpaths, add new markers.
    
    * feat: run ansible-lint on pull request.
    
    * feat!: remove template directory for git.
    
    * feat: setup merge tool for git.
    
    * feat: add testing of different directory.
    
    * feat: add pytest opts in file.
    
    * style: ignore global conftest.py file.
    
    * feat: add function to test file.

--- roles/home_config/tests/test_linux_home.py
+++ roles/home_config/tests/test_linux_home.py
@@ -2,18 +2,28 @@
 import pytest
 from testinfra.host import Host
 
+
 @pytest.mark.linux
 @pytest.mark.home_config
 class TestPackage:
-    @pytest.fixture
-    def get_home(self, host: Host):
-        return host.user('root').home
+    def test_home_dir(self, host: Host, get_home: str):
+        assert host.file(f"{get_home}/.bashrc.d").is_directory
 
-    def test_dir(self, host: Host, get_home: str):
-        assert host.file("/tmp/RAM").is_directory
-        assert host.file(get_home).is_directory
+        assert host.file(f"{get_home}/.ssh").is_directory
+        assert host.file(f"{get_home}/.gnupg").is_directory
+        assert host.file(f"{get_home}/.keys").is_directory
 
-    def test_ansible_mod(self, host: Host):
-        assert host.ansible(
-            "ansible.builtin.command", "echo foo", check=False
-        )["stdout"] == 'foo'
+        assert host.file(f"{get_home}/.local/bin").is_directory
+        assert host.file(f"{get_home}/.local/opt").is_directory
+        assert host.file(f"{get_home}/.local/script").is_directory
+        assert host.file(f"{get_home}/.local/state").is_directory
+
+        assert host.file(f"{get_home}/.config/systemd/user/").is_directory
+
+        assert host.file(f"{get_home}/Repos/git").is_directory
+
+    def test_home_file(self, host: Host, get_home: str):
+        assert host.file(f"{get_home}/.bashrc").is_file
+        assert host.file(f"{get_home}/.gitignore").is_file
+        assert host.file(f"{get_home}/.gitconfig").is_file
+        assert host.file(f"{get_home}/.bash_profile").is_file
