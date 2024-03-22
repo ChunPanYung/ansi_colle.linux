c9753e92c3b0befc4e8aacaa36db77c0fbf826c4
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2024-03-22 16:25:24 -0400

    refactor: directly put the file content into task file.

--- roles/home_config/files/bashrc.bash
+++
@@ -1,3 +0,0 @@
-# shellcheck shell=bash
-compopt -o nospace ls  # Do not add space after autocompletion for 'ls' command
-complete -o nospace -F _longopt source  # can't use it directly with compopt
